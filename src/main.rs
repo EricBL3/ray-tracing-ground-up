56a50eef46d750f5e901405e6c425c966a8791cb
EricBL3 <ericbuitron3@gmail.com>
2024-01-14 19:52:09 -0600

    Implement RayTracerWindow class

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,20 @@
+mod constants;
+mod ray_tracer_window;
+
 extern crate sdl2;
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
 use std::time::Duration;
 
+use ray_tracer_window::RayTracerWindow;
+
 pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    let mut window = RayTracerWindow::new(constants::SCREEN_WIDTH, constants::SCREEN_HEIGHT);
 
-    let window = video_subsystem
-        .window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
-    'running: loop {
+    while !window.should_close() {
         i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-        // The rest of the game loop goes here...
+        window.set_pixel(100, 100, i, 64, 255 - i);
 
-        canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
---
+++ src/ray_tracer_window.rs
@@ -0,0 +1,62 @@
+extern crate sdl2;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Point;
+use sdl2::render::*;
+use sdl2::video::*;
+use sdl2::Sdl;
+
+pub struct RayTracerWindow {
+    canvas: Canvas<Window>,
+    sdl_context: Sdl,
+}
+
+impl RayTracerWindow {
+    //Constructs and initializes the ray tracer window.
+    pub fn new(width: u32, height: u32) -> Self {
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+
+        let window = video_subsystem
+            .window("Ray Tracing from the Ground Up", width, height)
+            .position_centered()
+            .build()
+            .unwrap();
+
+        let mut canvas = window.into_canvas().build().unwrap();
+
+        canvas.set_draw_color(Color::BLACK);
+        canvas.clear();
+
+        Self {
+            canvas: canvas,
+            sdl_context: sdl_context,
+        }
+    }
+
+    //Function that draws a pixel of the specified RGB color at the specified location to the canvas.
+    pub fn set_pixel(&mut self, x: i32, y: i32, r: u8, g: u8, b: u8) {
+        self.canvas.set_draw_color(Color::RGB(r, g, b));
+        self.canvas.draw_point(Point::new(x, y)).unwrap();
+        self.canvas.present();
+    }
+
+    //Determines if the window should close depending on user input.
+    pub fn should_close(&self) -> bool {
+        let mut event_pump = self.sdl_context.event_pump().unwrap();
+        let mut result = false;
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => result = true,
+                _ => result = false,
+            }
+        }
+
+        result
+    }
+}
